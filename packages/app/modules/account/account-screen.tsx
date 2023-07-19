17d43092776389387bbc4e04205b0b8e93f7e7b8
rofi <rfigueroa@truelogic.io>
2023-07-19 19:48:25 -0400

    [web] style: bg color to white on order for native

--- packages/app/modules/account/account-screen.tsx
+++ packages/app/modules/account/account-screen.tsx
@@ -4,7 +4,7 @@ import OverviewTemplate from './templates/overview-template'
 export const AccountScreen = () => {
   return (
     <ScrollView
-      className={'web:bg-gray-50 ios:bg-white android:bg-white web:px-4'}
+      className={'ios:bg-white android:bg-white web:px-4'}
       keyboardShouldPersistTaps={'always'}
     >
       <OverviewTemplate />
