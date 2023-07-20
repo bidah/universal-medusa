a2eefdb1fd30eaf14f484d5232e483836a6de320
rofi <rfigueroa@truelogic.io>
2023-07-20 14:26:46 -0400

    style: spacing on order details between payment/delivery and need help/order summary

--- packages/app/modules/order/components/help/index.tsx
+++ packages/app/modules/order/components/help/index.tsx
@@ -1,21 +1,23 @@
-import React from "react"
-import {View, Pressable, Text} from 'app/design'
-import {Link} from "solito/link";
-import {textBaseRegular, textBaseSemi} from "../../../../design/tailwind/custom-css-classes";
-
+import React from 'react'
+import { View, Pressable, Text } from 'app/design'
+import { Link } from 'solito/link'
+import {
+  textBaseRegular,
+  textBaseSemi,
+} from '../../../../design/tailwind/custom-css-classes'
 
 const Help = () => {
   return (
-    <View>
+    <View className={'ios:mb-4'}>
       <Text className={textBaseSemi}>Need help?</Text>
-      <View className={ `${textBaseRegular} my-2 `}>
-        <View className="gap-y-2 flex flex-col">
-            <Link href="/contact">
-              <Text>Contact</Text>
-            </Link>
-            <Link href="/contact">
-                <Text>Returns & Exchanges</Text>
-            </Link>
+      <View className={`${textBaseRegular} my-2 `}>
+        <View className="flex flex-col gap-y-2">
+          <Link href="/contact">
+            <Text>Contact</Text>
+          </Link>
+          <Link href="/contact">
+            <Text>Returns & Exchanges</Text>
+          </Link>
         </View>
       </View>
     </View>
--- packages/app/modules/order/components/payment-details/index.tsx
+++ packages/app/modules/order/components/payment-details/index.tsx
@@ -1,7 +1,6 @@
-import { Payment, PaymentStatus } from "@medusajs/medusa"
-import {View, Text} from 'app/design'
-import {textBaseSemi} from "../../../../design/tailwind/custom-css-classes";
-
+import { Payment, PaymentStatus } from '@medusajs/medusa'
+import { View, Text } from 'app/design'
+import { textBaseSemi } from '../../../../design/tailwind/custom-css-classes'
 
 type PaymentDetailsProps = {
   payments: Payment[]
@@ -10,16 +9,16 @@ type PaymentDetailsProps = {
 
 const PaymentDetails = ({ payments, paymentStatus }: PaymentDetailsProps) => {
   return (
-    <View>
+    <View className={'ios:mb-4'}>
       <Text className={textBaseSemi}>Payment</Text>
       <View className="my-2">
         {payments.map((p) => {
           switch (p.provider_id) {
-            case "stripe":
+            case 'stripe':
               return <StripeDetails key={p.id} payment={p} />
-            case "paypal":
+            case 'paypal':
               return <PayPalDetails key={p.id} />
-            case "manual":
+            case 'manual':
               return <TestDetails key={p.id} />
             default:
               return null
@@ -32,7 +31,7 @@ const PaymentDetails = ({ payments, paymentStatus }: PaymentDetailsProps) => {
 
 const PayPalDetails = () => {
   return (
-    <View className="flex flex-col text-base-regular">
+    <View className="text-base-regular flex flex-col">
       <Text className="text-small-regular text-gray-700">PayPal</Text>
       <Text>PayPal payment</Text>
     </View>
@@ -48,14 +47,14 @@ const StripeDetails = ({ payment }: { payment: Payment }) => {
   } = (payment.data.charges as any).data[0].payment_method_details.card
 
   return (
-    <View className="flex flex-col text-base-regular">
+    <View className="text-base-regular flex flex-col">
       <Text className="text-small-regular text-gray-700">
         {card.brand.substring(0, 1).toUpperCase()}
         {card.brand.substring(1)}
       </Text>
       <Text>************{card.last4}</Text>
       <Text>
-        {card.exp_month > 9 ? card.exp_month : `0${card.exp_month}`} /{" "}
+        {card.exp_month > 9 ? card.exp_month : `0${card.exp_month}`} /{' '}
         {card.exp_year.toString().slice(2)}
       </Text>
     </View>
@@ -64,7 +63,7 @@ const StripeDetails = ({ payment }: { payment: Payment }) => {
 
 const TestDetails = () => {
   return (
-    <View className="flex flex-col text-base-regular">
+    <View className="text-base-regular flex flex-col">
       <Text className="text-small-regular text-gray-700">Test</Text>
       <Text>Test payment using medusa-payment-manual</Text>
     </View>
