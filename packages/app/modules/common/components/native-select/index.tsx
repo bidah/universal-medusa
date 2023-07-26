fd80497f8363d48b03e3e3747ae3eba21cff50e7
rofi <rfigueroa@truelogic.io>
2023-07-25 20:13:51 -0400

    s: fix: native: native-select border

--- packages/app/modules/common/components/native-select/index.tsx
+++ packages/app/modules/common/components/native-select/index.tsx
@@ -2,6 +2,7 @@ import { Text, View, Pressable, MotiView } from '../../../../design'
 import ChevronDown from '../../icons/chevron-down'
 import { BottomSheetModal, BottomSheetScrollView } from '@gorhom/bottom-sheet'
 import React, { useCallback, useMemo, useRef } from 'react'
+import { textBaseRegular } from '../../../../design/tailwind/custom-css-classes'
 
 const NativeSelect = React.forwardRef((props, bottomSheetRef) => {
   const { children } = props
@@ -13,17 +14,18 @@ const NativeSelect = React.forwardRef((props, bottomSheetRef) => {
 
   return (
     <View className={'pt-2'}>
-      <Pressable onPress={() => handlePresentModalPress()}>
-        <View className="text-base-regular relative flex items-center border border-gray-200">
-          <Text
-            {...props}
-            className="flex-1 appearance-none border-none bg-transparent px-4 py-2.5 outline-none transition-colors duration-150 focus:border-gray-700"
-          >
-            {props.value}
-          </Text>
-          <View className="pointer-events-none absolute inset-y-[25%] right-4 flex items-center">
-            <ChevronDown />
-          </View>
+      <Pressable
+        onPress={() => handlePresentModalPress()}
+        className={`${textBaseRegular} relative flex items-center border border-gray-200`}
+      >
+        <Text
+          {...props}
+          className="flex-1 appearance-none border-none bg-transparent px-4 py-2.5 outline-none transition-colors duration-150 focus:border-gray-700"
+        >
+          {props.value}
+        </Text>
+        <View className="pointer-events-none absolute inset-y-[25%] right-4 flex items-center">
+          <ChevronDown />
         </View>
       </Pressable>
       <View className={'mx-10'}>
