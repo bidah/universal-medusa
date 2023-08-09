69380bd42c3deba0c1d7a1f7b7345080d8eab141
rofi <rfigueroa@truelogic.io>
2023-08-09 11:48:34 -0400

    x

--- packages/app/modules/checkout/templates/index.tsx
+++ packages/app/modules/checkout/templates/index.tsx
@@ -32,7 +32,7 @@ const CheckoutTemplate = () => {
         {/*</View>*/}
         <View className="relative">
           {/*<CheckoutLoader />*/}
-          <View className="small:grid-cols-[1fr_416px] small:content-container small:grid xs:grid-cols-1 small:gap-y-8 small:gap-x-8 ios:pb-0 ios:pt-8 web:py-12">
+          <View className="small:grid-cols-[1fr_416px] small:content-container small:grid xs:grid-cols-1 small:gap-y-8 small:gap-x-8 ios:pb-0 ios:pt-0 web:py-12">
             <CheckoutForm />
             <CheckoutSummary />
           </View>
