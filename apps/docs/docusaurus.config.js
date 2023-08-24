eb2f67433f1f3be881ac48ec5801d36485a459ab
rofi <rfigueroa@truelogic.io>
2023-08-24 12:45:02 -0400

    x

--- apps/docs/docusaurus.config.js
+++ apps/docs/docusaurus.config.js
@@ -14,7 +14,7 @@ const config = {
   // Check for broken links. Set to 'ignore' to ignore broken links
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-test-site.com',
+  url: 'https://universal-medusa-docs.vercel.app/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
