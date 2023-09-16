5c9063c0692cc29010119d36cf07da57752a6a83
rofi <rfigueroa@truelogic.io>
2023-09-16 09:32:07 -0300

    port: add spinner

--- packages/app/modules/common/icons/spinner.tsx
+++ packages/app/modules/common/icons/spinner.tsx
@@ -1,37 +1,67 @@
-import React from 'react'
-import { Svg, Path, Circle } from 'app/design'
-import { IconProps } from 'app/types/icon'
+import React, { useEffect } from 'react'
+
+import { Text, StyleSheet, View } from 'react-native'
+
+import Animated, {
+  useAnimatedStyle,
+  useSharedValue,
+  withRepeat,
+  withTiming,
+  cancelAnimation,
+  Easing,
+} from 'react-native-reanimated'
+import { Circle } from 'react-native-svg'
 
 const Spinner: React.FC<IconProps> = ({
   size = '16',
   color = 'black',
   ...attributes
 }) => {
+  const rotation = useSharedValue(0)
+  const animatedStyles = useAnimatedStyle(() => {
+    return {
+      transform: [
+        {
+          rotateZ: `${rotation.value}deg`,
+        },
+      ],
+    }
+  }, [rotation.value])
+
+  useEffect(() => {
+    rotation.value = withRepeat(
+      withTiming(360, {
+        duration: 1000,
+        easing: Easing.linear,
+      }),
+      200
+    )
+    return () => cancelAnimation(rotation)
+  }, [])
+
   return (
-    <Svg
-      className="animate-spin"
-      width={size}
-      height={size}
-      xmlns="http://www.w3.org/2000/svg"
-      fill="none"
-      viewBox="0 0 24 24"
-      {...attributes}
-    >
-      <Circle
-        className="opacity-25"
-        cx="12"
-        cy="12"
-        r="10"
-        stroke={color}
-        strokeWidth="4"
-      ></Circle>
-      <Path
-        className="opacity-75"
-        fill={color}
-        d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-      ></Path>
-    </Svg>
+    <View style={styles.container}>
+      <Animated.View style={[styles.spinner, animatedStyles]} />
+    </View>
   )
 }
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: '100%',
+  },
+  spinner: {
+    height: 20,
+    width: 20,
+    borderRadius: 40,
+    borderWidth: 4,
+    borderTopColor: 'gray',
+    borderRightColor: 'gray',
+    borderBottomColor: 'gray',
+    borderLeftColor: 'lightgray',
+  },
+})
 
 export default Spinner
